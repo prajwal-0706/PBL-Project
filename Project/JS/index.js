721b8b7acea36e0e1c87b28e104e136ea04b1674
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-04-21 02:22:59 +0530

    Charts Done Successfully

--- Project/JS/index.js
+++ Project/JS/index.js
@@ -1,7 +1,9 @@
-//Plot Options
-let options = {
+const charts = document.querySelectorAll('.chart');
+
+//Plot Options for chart 1
+let options_1 = {
   chart: {
-    type: 'line',
+    type: 'bar',
   },
   series: [
     {
@@ -9,7 +11,7 @@ let options = {
       data: [10, 16, 37, 17, 38],
     },
     {
-      name: 'Prajwal',
+      name: 'Second Population',
       data: [50, 20, 39, 40, 49],
     },
   ],
@@ -18,10 +20,106 @@ let options = {
   },
 };
 
-//Add Changes
+//Add Changes for chart 1
 
-const chart = new ApexCharts(document.querySelector('.chart'), options);
+const chart = new ApexCharts(charts[0], options_1);
 
-//Render changes
+//Render changes for chart 1
 
 chart.render();
+
+// Plot Options for chart-2
+
+let options_2 = {
+  chart: {
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Total Count',
+      data: [20, 30, 40, 50, 10],
+    },
+    {
+      name: 'Another Count',
+      data: [12, 45, 67, 8, 12],
+    },
+  ],
+  xaxis: {
+    categories: ['p', 'r', 'a', 'j', 'w'],
+  },
+};
+
+//Add Changes for chart-2
+
+const chart_1 = new ApexCharts(charts[1], options_2);
+
+//Render changes for chart-2
+chart_1.render();
+
+//Plot Options for chart-3
+
+let option_3 = {
+  chart: {
+    type: 'donut',
+  },
+  series: [12, 34, 56, 67, 54],
+  plotOptions: {
+    labels: ['Apple', 'Watermelon', 'Mango', 'Coconut', 'Banana'],
+    pie: {
+      customScale: 0.8,
+      donut: {
+        labels: {
+          show: true,
+        },
+      },
+    },
+  },
+};
+
+//Add changes for chart-3
+
+const chart_3 = new ApexCharts(charts[2], option_3);
+
+//Render changes for chart-3
+
+chart_3.render();
+
+//Plot Options for chart-4
+
+let option_4 = {
+  chart: {
+    type: 'area',
+  },
+  series: [
+    {
+      name: 'Area Chart',
+      data: [60, 40, 20, 60, 50],
+    },
+  ],
+  dataLabels: {
+    enabled: false,
+  },
+  colors: ['#00BA6C'],
+  fill: {
+    gradient: {
+      enabled: true,
+    },
+  },
+  xaxis: {
+    categories: ['D', 'U', 'J', 'A', 'L'],
+  },
+  // grid: {
+  //   yaxis: {
+  //     lines: {
+  //       show: false,
+  //     },
+  //   },
+  // },
+};
+
+//Add changes for chart-4
+
+const chart_4 = new ApexCharts(charts[3], option_4);
+
+//Render changes for chart-4
+chart_4.render();
